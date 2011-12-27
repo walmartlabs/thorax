216a24b2c6c2ae0ef31ff52c6464d8845ae696c2
Joe Hudson <joehud@gmail.com>
2011-12-27 10:15:27 -0500

    removed build/watch scripts

--- examples/todo/build.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-node_modules/lumbar/bin/lumbar --config config/dev.json lumbar.json public
--- examples/todo/watch.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-node_modules/lumbar/bin/lumbar --watch --config config/dev.json lumbar.json public
