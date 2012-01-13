e9ce9ed52c3805236f68cd5b54dd43a62b056b56
kpdecker <kpdecker@gmail.com>
2012-01-13 14:57:53 -0800

    Add client-side highlighting to docs

--- docs/scripts/whitespace.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-  var pres = document.getElementsByTagName('pre');
-  for (var i = 0; i < pres.length; ++i) {
-    var pre = pres[i];
-    pre.innerHTML = pre.innerHTML.replace(/(^[\s\n]+)/m,'');
-  } 
-};
