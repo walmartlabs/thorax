24c2eaddd3ce3ac11feffba2fd2c43efd70d22c6
Ryan Eastridge <ryan@syntacticx.com>
2012-04-15 14:52:31 -0700

    working model view binding test

--- test/test.js
+++ test/test.js
@@ -1,4 +1,31 @@
+$(function() {
 
-test("True", function() {
-  ok(true);
+  Thorax.configure({
+    templatePathPrefix: ''
+  });
+  Application.templates = {
+    'letter-item.handlebars': Handlebars.compile("<li>{{letter}}</li>")
+  };
+
+  var LetterModel = Thorax.Model.extend({});
+  LetterModel.letters = ['a','b','c','d'].map(function(letter) {
+    return new LetterModel({letter: letter});
+  });
+
+  var LetterItemView = Thorax.View.extend({
+    name: 'letter-item'
+  });
+
+  test("Model View binding", function() {
+    var a = new LetterItemView({
+      model: LetterModel.letters[0]
+    });
+    console.log(a.el);
+    equal(a.el.firstChild.innerHTML, 'a');
+    var b = new LetterItemView();
+    b.setModel(LetterModel.letters[1]);
+    equal(b.el.firstChild.innerHTML, 'b');
+  });
+
 });
+
