3f2152e38f4ee2fe68bc6bc71ee7fa49ecea0a0b
Ryan Eastridge <ryan@eastridge.xxx>
2012-09-22 10:14:40 +0200

    cleaner mixin injection

--- src/mixin.js
+++ src/mixin.js
@@ -1,16 +1,7 @@
 {{#inject "configure"}}
   //HelperView will not have mixins so need to check
-  if (this.constructor.mixins) {
-    //mixins
-    for (var i = 0; i < this.constructor.mixins.length; ++i) {
-      applyMixin.call(this, this.constructor.mixins[i]);
-    }
-    if (this.mixins) {
-      for (var i = 0; i < this.mixins.length; ++i) {
-        applyMixin.call(this, this.mixins[i]);
-      }
-    }
-  }
+  this.constructor.mixins && _.each(this.constructor.mixins, applyMixin, this);
+  this.mixins && _.each(this.mixins, applyMixin, this);
 {{/inject}}
 
 {{#inject "extend"}}
