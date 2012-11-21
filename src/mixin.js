333dd0351c54f48d9954072b74265d40b7cbfd20
Ryan Eastridge <ryan@eastridge.xxx>
2012-11-21 14:31:32 -0800

    Revert mixin behavior and API to 1.x

--- src/mixin.js
+++ src/mixin.js
@@ -1,7 +1,9 @@
+Thorax.Mixins = {};
+
 {{#inject "configure"}}
   //HelperView will not have mixins so need to check
-  this.constructor.mixins && _.each(this.constructor.mixins, applyMixin, this);
-  this.mixins && _.each(this.mixins, applyMixin, this);
+  this.constructor.mixins && _.each(this.constructor.mixins, this.mixin, this);
+  this.mixins && _.each(this.mixins, this.mixin, this);
 {{/inject}}
 
 {{#inject "extend"}}
@@ -12,8 +14,32 @@ _.extend(Thorax.View, {
   mixins: [],
   mixin: function(mixin) {
     this.mixins.push(mixin);
+  },
+  registerMixin: function(name, callback, methods) {
+    Thorax.Mixins[name] = [callback, methods];
   }
 });
+
+Thorax.View.prototype.mixin = function(name) {
+  if (!this._appliedMixins) {
+    this._appliedMixins = [];
+  }
+  if (this._appliedMixins.indexOf(name) == -1) {
+    this._appliedMixins.push(name);
+    if (typeof name === 'function') {
+      name.call(this);
+    } else {
+      var mixin = Thorax.Mixins[name];
+      _.extend(this, mixin[1]);
+      //mixin callback may be an array of [callback, arguments]
+      if (_.isArray(mixin[0])) {
+        mixin[0][0].apply(this, mixin[0][1]);
+      } else {
+        mixin[0].apply(this, _.toArray(arguments).slice(1));
+      }
+    }
+  }
+};
 
 function applyMixin(mixin) {
   if (_.isArray(mixin)) {
