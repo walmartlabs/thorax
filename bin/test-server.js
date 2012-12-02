de657acb5f6f9d47854bdfb1c2032673e30fba73
Ryan Eastridge <ryan@eastridge.xxx>
2012-12-02 14:36:25 -0800

    Update test runner to actually work

--- bin/test-server.js
+++ bin/test-server.js
@@ -1,13 +1,30 @@
 // Shamelessly pulled from Modernizr
 // https://github.com/Modernizr/Modernizr/blob/master/test/js/server.js
-var connect = require('connect'),
+var express = require('express'),
   path = require('path'),
+  fs = require('fs'),
 	args = process.argv.slice(2),
-	folder = path.join(args[0] ? (process.cwd() + '/' + args[0]) : (__dirname + '/../../'), 'test'),
+  root = path.join(__dirname, '..');
+  folder = path.join(root, 'test'),
 	port = args[1] || '80';	
 
-var server = connect.createServer(
-    connect.static(folder)
-).listen(port);
+var server = express.createServer();
+server.use(express.static(folder));
+
+function serveFile(file) {
+  server.get('/' + file, function(request, response) {
+    response.sendfile(path.join(root, file));
+  });
+}
+
+fs.readdirSync(path.join(root, 'lib')).forEach(function(item) {
+  serveFile(path.join('lib', item));
+});
+
+fs.readdirSync(path.join(root, 'dist')).forEach(function(item) {
+  serveFile(path.join('dist', item));
+});
+
+server.listen(port);
 
 console.log("Server started on port %s in %s", port, folder);
