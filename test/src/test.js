4f943f00991fc4d2a2a8289b29ee1f0072305d0c
Ryan Eastridge <ryan@eastridge.xxx>
2012-12-20 12:05:58 -0800

    Pass options to super helper template call

--- test/src/test.js
+++ test/src/test.js
@@ -242,6 +242,16 @@ describe('core', function() {
     child.render();
     expect(child.$('.parent').length).to.equal(1);
     expect(child.$('.child').length).to.equal(1);
+
+    parent = Thorax.View.extend({
+      template: '{{#collection letters tag="ul"}}<li>{{letter}}</li>{{/collection}}'
+    });
+    var instance = new (parent.extend({
+      template: '{{super}}'
+    }))({letters: new Thorax.Collection([{letter: 'a'}])});
+    instance.render();
+    expect(instance.$('li').length).to.equal(1);
+    expect(instance.$('li').eq(0).html()).to.equal('a');
   });
 
   it("template yield", function() {
