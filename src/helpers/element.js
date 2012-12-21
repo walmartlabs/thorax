4622c4e97e698f8eeaeb27322c258209531a1a6c
Ryan Eastridge <ryan@eastridge.xxx>
2012-12-21 13:21:11 -0800

    Remove elements before innerHTML is set for IE

--- src/helpers/element.js
+++ src/helpers/element.js
@@ -13,9 +13,11 @@ Handlebars.registerHelper('element', function(element, options) {
 // IE will lose a reference to the elements if view.el.innerHTML = '';
 // If they are removed one by one the references are not lost
 Thorax.View.on('before:append', function() {
-  _.each(this._elementsByCid, function(element, cid) {
-    $(element).remove();
-  });
+  if (this._renderCount > 0) {
+    _.each(this._elementsByCid, function(element, cid) {
+      $(element).remove();
+    });
+  }
 });
 
 Thorax.View.on('append', function(scope, callback) {
