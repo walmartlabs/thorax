cb9430ed1c11a7caf752aac40d20160aedb5fa07
Ryan Eastridge <ryan@eastridge.xxx>
2012-12-27 15:51:24 -0800

    Simplify call to renderTemplate in template helper

--- src/helpers/template.js
+++ src/helpers/template.js
@@ -1,6 +1,6 @@
 Handlebars.registerHelper('template', function(name, options) {
   var context = _.extend({fn: options && options.fn}, this, options ? options.hash : {});
-  var output = Thorax.View.prototype.renderTemplate.call(getOptionsData(options).view, name, context);
+  var output = getOptionsData(options).view.renderTemplate(name, context);
   return new Handlebars.SafeString(output);
 });
 
