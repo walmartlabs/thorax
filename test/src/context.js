eb601d9a3b6012ad88e89b607133bba79084537c
Ryan Eastridge <ryan@eastridge.xxx>
2012-12-28 12:03:46 -0800

    Add test for render option

--- test/src/context.js
+++ test/src/context.js
@@ -6,4 +6,23 @@ describe("context", function() {
     view.render();
     expect(view.html()).to.equal('value');
   });
+
+  it("render option should re-render view", function() {
+    var view = new (Thorax.View.extend({
+      template: '{{key}}'
+    }));
+    view.render();
+    expect(view.html()).to.equal('');
+    view.set({key: 'value'});
+    expect(view.html()).to.equal('');
+    view.render();
+    expect(view.html()).to.equal('value');
+    view.set({key: 'value2'}, {render: true});
+    expect(view.html()).to.equal('value2');
+    // Keep changing to ensure "render" option is not stored
+    view.set('key', 'value');
+    expect(view.html()).to.equal('value2');
+    view.render();
+    expect(view.html()).to.equal('value');
+  });
 });
