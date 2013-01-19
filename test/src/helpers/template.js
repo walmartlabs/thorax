c1c4aa281c321f5306924f6a715d2965d701a190
kpdecker <kpdecker@gmail.com>
2013-01-19 02:41:57 -0600

    Template helper test file

---
+++ test/src/helpers/template.js
@@ -0,0 +1,36 @@
+describe('template helper', function() {
+  it("template function can be specified", function() {
+    var childReturningString = new Thorax.View({
+      template: function(data, options) {
+        expect(options.data.cid).to.match(/^t/);
+        return 'template';
+      }
+    });
+    childReturningString.render();
+    expect(childReturningString.html()).to.equal('template');
+    var childReturningElement = new Thorax.View({
+      template: function() {
+        return $('<p>template</p>')[0];
+      }
+    });
+    childReturningElement.render();
+    expect(childReturningElement.$('p').html()).to.equal('template');
+    var childReturning$ = new Thorax.View({
+      template: function() {
+        return $('<p>template</p>');
+      }
+    });
+    childReturning$.render();
+    expect(childReturning$.$('p').html()).to.equal('template');
+  });
+
+  it("template yield", function() {
+    Thorax.templates['yield-child'] = '<span>{{@yield}}</span>';
+    Thorax.templates['yield-parent'] = '<p>{{#template "yield-child"}}content{{/template}}</p>';
+    var view = new Thorax.View({
+      name: 'yield-parent'
+    });
+    view.render();
+    expect(view.$('p > span').html()).to.equal('content');
+  });
+});
--- test/src/thorax.js
+++ test/src/thorax.js
@@ -97,41 +97,6 @@ describe('core', function() {
     $('#test-target-container').remove();
   });
 
-  it("template function can be specified", function() {
-    var childReturningString = new Thorax.View({
-      template: function(data, options) {
-        expect(options.data.cid).to.match(/^t/);
-        return 'template';
-      }
-    });
-    childReturningString.render();
-    expect(childReturningString.html()).to.equal('template');
-    var childReturningElement = new Thorax.View({
-      template: function() {
-        return $('<p>template</p>')[0];
-      }
-    });
-    childReturningElement.render();
-    expect(childReturningElement.$('p').html()).to.equal('template');
-    var childReturning$ = new Thorax.View({
-      template: function() {
-        return $('<p>template</p>');
-      }
-    });
-    childReturning$.render();
-    expect(childReturning$.$('p').html()).to.equal('template');
-  });
-
-  it("template yield", function() {
-    Thorax.templates['yield-child'] = '<span>{{@yield}}</span>';
-    Thorax.templates['yield-parent'] = '<p>{{#template "yield-child"}}content{{/template}}</p>';
-    var view = new Thorax.View({
-      name: 'yield-parent'
-    });
-    view.render();
-    expect(view.$('p > span').html()).to.equal('content');
-  });
-
   it("should allow local helpers to be declared", function() {
     // register a global helper to ensure that it isn't overwritten
     Handlebars.registerHelper('globalHelper', function() {
