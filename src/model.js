534c7e40ca3c7d990af84c843d05ae7f02fa9249
kpdecker <kpdecker@gmail.com>
2013-01-21 15:57:37 -0600

    Fix model empty handling

--- src/model.js
+++ src/model.js
@@ -1,14 +1,14 @@
-/*global createRegistryWrapper, dataObject */
+/*global createRegistryWrapper, dataObject, getValue */
 var modelCidAttributeName = 'data-model-cid';
 
 Thorax.Model = Backbone.Model.extend({
   isEmpty: function() {
-    return this.isPopulated();
+    return !this.isPopulated();
   },
   isPopulated: function() {
     // We are populated if we have attributes set
-    var attributes = _.clone(this.attributes);
-    var defaults = _.isFunction(this.defaults) ? this.defaults() : (this.defaults || {});
+    var attributes = _.clone(this.attributes),
+        defaults = getValue(this, 'defaults') || {};
     for (var default_key in defaults) {
       if (attributes[default_key] != defaults[default_key]) {
         return true;
