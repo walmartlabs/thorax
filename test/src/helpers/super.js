faa99a1b2386db974b0c11890c9cdf1b5397c293
Ryan Eastridge <ryan@eastridge.me>
2013-02-08 16:08:26 -0800

    Simplify logic of super helper

--- test/src/helpers/super.js
+++ test/src/helpers/super.js
@@ -13,6 +13,17 @@ describe('super helper', function() {
     expect(child.$('.parent').length).to.equal(1);
     expect(child.$('.child').length).to.equal(1);
 
+    // same as above but with template attr using name
+    parent = Thorax.View.extend({
+      template: 'super-named-test'
+    });
+    child = new (parent.extend({
+      template: Handlebars.compile('<div class="child"></div>{{super}}')
+    }))();
+    child.render();
+    expect(child.$('.parent').length).to.equal(1);
+    expect(child.$('.child').length).to.equal(1);
+
     parent = Thorax.View.extend({
       name: 'super-test',
       template: Handlebars.compile('<div class="parent"></div>')
