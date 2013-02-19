f3ea1bd0d3a7206b5171971fac770a2e09fe7cf8
Ryan Eastridge <ryan@eastridge.me>
2013-02-19 15:15:43 -0800

    Remove erroneous task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,5 @@
 module.exports = function(grunt) {
   grunt.loadTasks('tasks');
 
-  grunt.initConfig({
-    thorax: {
-      templates: {
-        source: '../thorax-boilerplate/release/lumbar/templates/',
-        target: '../thorax-boilerplate/release/lumbar/templates.js',
-        applicationName: 'Application'
-      }
-    }
-  });
-
   grunt.registerTask('default', ['thorax:release']);
 };
