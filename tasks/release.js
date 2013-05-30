9fec5b7ba0adcd0eaa35b5e6771d970382369297
Ryan Eastridge <ryan@eastridge.me>
2013-05-30 15:12:54 -0700

    Update release task

--- tasks/release.js
+++ tasks/release.js
@@ -11,18 +11,18 @@ try {
 }
 
 var config = {
-  "thorax.js": [
-    'lib/jquery.js',
-    'lib/handlebars.js',
-    'lib/underscore.js',
-    'lib/backbone.js',
+  "thorax-combined.js": [
+    'components/jquery/jquery.js',
+    'components/handlebars/handlebars.js',
+    'components/underscore/underscore.js',
+    'components/backbone/backbone.js',
     'build/dev/thorax.js'
   ],
-  "thorax-mobile.js": [
-    'lib/zepto.js',
-    'lib/handlebars.js',
-    'lib/underscore.js',
-    'lib/backbone.js',
+  "thorax-combined-mobile.js": [
+    'components/zepto/zepto.js',
+    'components/handlebars/handlebars.runtime.js',
+    'components/underscore/underscore.js',
+    'components/backbone/backbone.js',
     'build/dev/thorax-mobile.js'
   ]
 };
@@ -40,10 +40,9 @@ module.exports = function(grunt) {
   grunt.registerTask('thorax:release', function() {
     var done = this.async();
     exec('jake lumbar', function(error, stdout, stderr) {
-      error && process.stdout.write(error);
+      error && process.stdout.write(error.toString());
       stdout && process.stdout.write(stdout);
       stderr && process.stdout.write(stderr);
-  
       for (var target in config) {
         var fileList = config[target],
             output = '';
@@ -59,7 +58,25 @@ module.exports = function(grunt) {
         console.log("Wrote: " + targetFile);
         console.log("Wrote: " + targetFile.replace(/\.js$/, '.min.js'));
       }
-      done(true);
+      var thoraxSrc = path.join(__dirname, '../build/dev/thorax.js'),
+          thoraxMobileSrc = path.join(__dirname, '../build/dev/thorax-mobile.js'),
+          thoraxDest = path.join(__dirname, targetDir, 'thorax.js'),
+          thoraxMobileDest = path.join(__dirname, targetDir, 'thorax-mobile.js');
+      var copyCommand = 
+        'cp ' + thoraxSrc + ' ' + thoraxDest + ';' +
+        'cp ' + thoraxMobileSrc + ' ' + thoraxMobileDest + ';';
+      exec(copyCommand, function(error, stdout, stderr) {
+        error && process.stdout.write(error.toString());
+        stdout && process.stdout.write(stdout);
+        stderr && process.stdout.write(stderr);
+        fs.writeFileSync(thoraxDest.replace(/\.js$/, '.min.js'), minify(fs.readFileSync(thoraxDest).toString()));
+        fs.writeFileSync(thoraxMobileDest.replace(/\.js$/, '.min.js'), minify(fs.readFileSync(thoraxMobileDest).toString()));
+        console.log("Wrote: " + thoraxDest);
+        console.log("Wrote: " + thoraxMobileDest);
+        console.log("Wrote: " + thoraxDest.replace(/\.js$/, '.min.js'));
+        console.log("Wrote: " + thoraxMobileDest.replace(/\.js$/, '.min.js'));
+        done(true);
+      });
     });
   });
 };
