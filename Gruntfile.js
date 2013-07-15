ab6e2a41c2f545d068feec2ceea81981917f8a78
Ryan Eastridge <ryan@eastridge.me>
2013-07-15 16:39:44 -0700

    Move code to separate grunt plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,26 @@
 module.exports = function(grunt) {
   grunt.loadTasks('tasks');
   grunt.loadNpmTasks('grunt-release');
+  grunt.loadNpmTasks('grunt-release-component');
+  grunt.initConfig({
+    'release-component': {
+      options: {
+        componentRepo: 'git@github.com:components/thorax.git',
+        copy: {
+          'build/release/thorax-combined.js': 'thorax-combined.js',
+          'build/release/thorax-combined.min.js': 'thorax-combined.min.js',
+          'build/release/thorax-combined-mobile.js': 'thorax-combined-mobile.js',
+          'build/release/thorax-combined-mobile.min.js': 'thorax-combined-mobile.min.js',
+          'build/release/thorax.js': 'thorax.js',
+          'build/release/thorax-mobile.js': 'thorax-mobile.js',
+          'build/release/thorax.min.js': 'thorax.min.js',
+          'build/release/thorax-mobile.min.js': 'thorax-mobile.min.js'
+        }
+      }
+    }
+  });
+
   ['major', 'minor', 'patch', 'prerelease'].forEach(function(type) {
-    grunt.registerTask('thorax:release:' + type, ['thorax:build', 'release:' + type, 'thorax:bower-release:' + type]);
+    grunt.registerTask('thorax:release:' + type, ['thorax:build', 'release-component:' + type]);
   });
 };
