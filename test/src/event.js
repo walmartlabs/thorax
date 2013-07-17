913b6cef8958ad56292224eb94a62ec1ac07d8a4
kpdecker <kpdecker@gmail.com>
2013-07-17 13:32:41 -0500

    Use listenTo rather than on for View contexts
    This allows views to automatically sever the connection between each
    other on destroy.

--- test/src/event.js
+++ test/src/event.js
@@ -374,6 +374,26 @@ describe('event', function() {
       $el.find('a').trigger('mousedown');
       expect(spy).to.have.been.calledTwice;
     });
+    it('should cleanup listening view events on release', function() {
+      var spy = this.spy(),
+          View = Thorax.View.extend({
+            template: function() {}
+          }),
+          view = new View(),
+          other = new View();
+
+      other.on('foo', spy, view);
+      other.trigger('foo');
+
+      expect(spy)
+          .to.have.been.calledOnce
+          .to.have.been.calledOn(view);
+
+      view.release();
+
+      other.trigger('foo');
+      expect(spy).to.have.been.calledOnce;
+    });
     it('should cleanup own view events on release', function() {
       var spy = this.spy(),
           View = Thorax.View.extend({
