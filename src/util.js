344e5d5e58adf6cdf5d2f764dcc4fc85d6bc8e89
kpdecker <kpdecker@gmail.com>
2014-01-12 15:21:05 -0600

    Handle void tags properly in Thorax.Util.tag

--- src/util.js
+++ src/util.js
@@ -219,6 +219,21 @@ function normalizeHTMLAttributeOptions(options) {
   }
 }
 
+var voidTags;
+function isVoidTag(tag) {
+  if (!voidTags) {
+    // http://www.w3.org/html/wg/drafts/html/master/syntax.html#void-elements
+    var tags = 'area,base,br,col,embed,hr,img,input,keygen,link,menuitem,meta,param,source,track,wbr';
+
+    voidTags = {};
+    _.each(tags.split(','), function(tag) {
+      voidTags[tag] = true;
+    });
+  }
+
+  return voidTags[tag];
+};
+
 Thorax.Util = {
   getViewInstance: function(name, attributes) {
     var ViewClass = Thorax.Util.getViewClass(name, true);
@@ -310,8 +325,14 @@ Thorax.Util = {
   },
   tag: function(attributes, content, scope) {
     var htmlAttributes = _.omit(attributes, 'tagName'),
-        tag = attributes.tagName || 'div';
-    return '<' + tag + ' ' + _.map(htmlAttributes, function(value, key) {
+        tag = attributes.tagName || 'div',
+        noClose = isVoidTag(tag);
+
+    if (noClose && content) {
+      throw new Error(createErrorMessage('void-tag-content'));
+    }
+
+    var openingTag = '<' + tag + ' ' + _.map(htmlAttributes, function(value, key) {
       if (_.isUndefined(value) || key === 'expand-tokens') {
         return '';
       }
@@ -320,6 +341,12 @@ Thorax.Util = {
         formattedValue = Thorax.Util.expandToken(value, scope);
       }
       return (key === 'className' ? 'class' : key) + '="' + Handlebars.Utils.escapeExpression(formattedValue) + '"';
-    }).join(' ') + '>' + (_.isUndefined(content) ? '' : content) + '</' + tag + '>';
+    }).join(' ') + '>';
+
+    if (noClose) {
+      return openingTag;
+    } else {
+      return openingTag + (_.isUndefined(content) ? '' : content) + '</' + tag + '>';
+    }
   }
 };
---
+++ test/src/util.js
@@ -0,0 +1,16 @@
+describe('util', function() {
+  describe('#tag', function() {
+    it('should handle close tags', function() {
+      expect(Thorax.Util.tag({tagName: 'div'})).to.equal('<div ></div>');
+      expect(Thorax.Util.tag({tagName: 'div'}, 'foo')).to.equal('<div >foo</div>');
+    });
+    it('should handle void tags', function() {
+      expect(Thorax.Util.tag({tagName: 'img'})).to.equal('<img >');
+    });
+    it('should throw on void tag with content', function() {
+      expect(function() {
+        Thorax.Util.tag({tagName: 'hr'}, 'something');
+      }).to.throw(/void-tag-content/);
+    });
+  });
+});
