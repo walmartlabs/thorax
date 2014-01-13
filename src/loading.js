62ec4557a0abf7ee538fa34b813a3d447ae02bfb
Joe Hudson <joehud@gmail.com>
2014-01-13 14:06:08 -0500

    fetchQueue null check https://github.com/walmartlabs/thorax/issues/304

--- src/loading.js
+++ src/loading.js
@@ -318,8 +318,9 @@ function fetchQueue(options, $super) {
     // Handle callers that do not pass in a super class and wish to implement their own
     // fetch behavior
     if ($super) {
-      this.fetchQueue._promise = $super.call(this, options);
-      return this.fetchQueue._promise;
+      var promise = $super.call(this, options);
+      this.fetchQueue && this.fetchQueue._promise = promise;
+      return promise;
     } else {
       return options;
     }
