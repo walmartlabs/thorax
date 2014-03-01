e83e2cad3b239d61d9a50e6ff04fbff4e9199de9
kpdecker <kpdecker@gmail.com>
2014-02-28 18:18:33 -0600

    Implement reusable uniqueId generator
    The default implementation either runs into conflicts due to generating the same id on both the client and the server or could error in a shared page environment due to Number overflow.

---
+++ src/server-side.js
@@ -0,0 +1,9 @@
+
+// Override uniqueId to ensure uniqueness across both the server and client
+// rendering cycles
+window._idCounter = window._idCounter || 0;
+
+_.uniqueId = function(prefix) {
+  var id = (window._reqId || '') + (++window._idCounter);
+  return prefix ? prefix + id : id;
+};
