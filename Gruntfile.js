6cad83528f91837acafda79487c47e3de5cbbb79
kpdecker <kpdecker@gmail.com>
2014-03-04 15:27:01 -0600

    Use grunt for primary travis test exec

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,9 +8,69 @@ module.exports = function(grunt) {
         'src/**/*.js'
       ]
     },
+
+    clean: ['build'],
+
+    'phoenix-build': {
+      dev: {
+        options: {
+          lumbarFile: 'build.json',
+          build: true,
+          output: 'build/dev'
+        }
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          base: 'build/dev',
+          hostname: '*',
+          port: 9999
+        }
+      }
+    },
+
+    'mocha_phantomjs': {
+      quick: {
+        options: {
+          urls: [
+            'http://localhost:9999/jquery/test.html',
+            'http://localhost:9999/zepto/test.html'
+          ]
+        }
+      },
+      legacy: {
+        options: {
+          urls: [
+            'http://localhost:9999/jquery-backbone-1-0/test.html',
+            'http://localhost:9999/zepto-backbone-1-0/test.html'
+          ]
+        }
+      }
+    },
+
+    watch: {
+      scripts: {
+        options: {
+          atBegin: true
+        },
+
+        files: ['src/**/*.js', 'test/**/*.js'],
+        tasks: ['jshint', 'phoenix-build:dev', 'mocha_phantomjs:quick']
+      }
+    }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-mocha-phantomjs');
+  grunt.loadNpmTasks('phoenix-build');
 
   grunt.loadTasks('tasks');
+
+  grunt.registerTask('test', ['clean', 'connect', 'jshint', 'phoenix-build', 'mocha_phantomjs']);
+  grunt.registerTask('dev', ['clean', 'connect', 'watch']);
 };
