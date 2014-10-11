926659aae613dea12f0eb773261cb03bbff35594
kpdecker <kpdecker@gmail.com>
2014-10-11 12:01:12 -0500

    Optimize template context generation
    Avoid cloning context when otherwise unnecessary.

--- src/helpers/template.js
+++ src/helpers/template.js
@@ -1,6 +1,21 @@
 /*global getOptionsData */
 Handlebars.registerHelper('template', function(name, options) {
-  var context = _.extend({fn: options && options.fn}, this, options ? options.hash : {});
+  /*jshint -W089 */
+  var hasHash = false;
+  for (var name in options.hash) {
+    // Not doing hasOwnProperty check here as this is going to be a handlebars
+    // generated object literal in most cases and under the rare situation that
+    // the Object prototype has manipulated, the extend path will continue to do
+    // the correct thing.
+    hasHash = true;
+    break;
+  }
+
+  var context = this;
+  if (options.fn || hasHash) {
+    context = _.extend({fn: options && options.fn}, context, options.hash);
+  }
+
   var output = getOptionsData(options).view.renderTemplate(name, context);
   return new Handlebars.SafeString(output);
 });
